0f7bb5ef65e8b50f2960e1d8de7199ee6c06d2eb
FernandoHuilca <fernando.huilca@epn.edu.ec>
2023-07-21 01:37:51 -0500

    Subiendo modificaciones a los tallers y testing

--- Primer Semestre/Testing/prueba.cpp
+++ Primer Semestre/Testing/prueba.cpp
@@ -1,8 +1,38 @@
+//Escriba un programa que pregunte al usuario un numero entero y positivo n y sumar los numeros desde 1 hasta n (use while y for )
 #include <iostream>
 using namespace std; 
+#include <fstream> 
+// Funcion que suma los numeros del archivo 
+void sumatorio (ifstream &archivo, double &suma); 
+
 int main ()
 {
-    cout << "Hello Wordl!! " << endl; 
+    double resultado ; 
+    ifstream archivo; 
+    archivo.open("datos.txt"); 
+    if (!archivo.is_open())
+    {
+        cout << "Archivo no abierto. " << endl; 
+    } 
+    else 
+    {
+        sumatorio(archivo, resultado);
+        cout << " El resultado de la suma de los valores es de: " << resultado << endl; 
+        archivo.close(); 
+    }
+
     return 0; 
 
+}
+
+void sumatorio (ifstream &archivo, double &suma)
+{
+    double dato; 
+    archivo >> dato; 
+    suma = 0 ; 
+    while (dato !=-1)
+    {
+        suma = suma + dato; 
+        archivo >> dato; 
+    }
 }
