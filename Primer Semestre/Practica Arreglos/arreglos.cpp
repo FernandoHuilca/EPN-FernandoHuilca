55f107ead8ab8496ad233e1ff00b4dedfc9659bd
FernandoHuilca <fernando.huilca@epn.edu.ec>
2023-08-03 13:22:12 -0500

    Subiendo ejercicio 4 resuelto

--- Primer Semestre/Practica Arreglos/arreglos.cpp
+++ Primer Semestre/Practica Arreglos/arreglos.cpp
@@ -6,22 +6,24 @@ using namespace std;
 #include <iomanip> 
 #include <random> //para ejercicio 4
 
+int const DIM_4 = 11;
+
 // Prototipos de Funciones
 int menu();
 //Generalmente se va a usar para imprimir en consola:
-void imprimir (int array []);
+void imprimir(int array[DIM_4]);
 
 
-void saludo_Ejercicio();
-void ejercicio_4 ();
+void saludo_Ejercicio(int opcion);
+void ejercicio_4();
 int aleatorios(); // para ejercicio 4 
 
 
 
 
 int main() {
-    cout << "_______BIENVENIDO A LOS EJERCICIOS DE FUNCIONES______" << endl;
-    cout << setw(54) << right <<  "~ AUTHOR Fernando Huilca" << endl;
+    cout << "       BIENVENIDO A LOS EJERCICIOS DE FUNCIONES      " << endl;
+    cout << setw(54) << right << "~ AUTHOR Fernando Huilca" << endl;
     cout << endl;
     int opcion = 1;
     while (opcion != 0) // Para presentar el menu varias veces
@@ -31,20 +33,20 @@ int main() {
         switch (opcion) {
         case 1:
             saludo_Ejercicio(opcion);
-        
+
             break;
         case 2:
             saludo_Ejercicio(opcion);
-        
+
             break;
         case 3:
             saludo_Ejercicio(opcion);
-        
+
             break;
         case 4:
             saludo_Ejercicio(opcion);
-            ejercicio_4 ();
-        
+            ejercicio_4();
+
             break;
         }
     }
@@ -57,14 +59,14 @@ int menu() {
     int opcion;
     do {
         cout << setw(41) << right << "MENU EJERCICIOS REALIZADOS" << endl;
-        cout << setw (40) << right << "-------------------------" << endl;
+        cout << setw(40) << right << "-------------------------" << endl;
         cout << endl;
         cout << "1. Funcion Exponencial" << endl;
         cout << "2. Funcion Segundo mas" << endl;
         cout << "3. Tiempo en segundos" << endl;
         cout << "4. Dados" << endl;
         cout << "0. Salir" << endl;
-        cout << endl; 
+        cout << endl;
         cout << "Ingrese una opcion: ";
 
         cin >> opcion;
@@ -81,49 +83,46 @@ void saludo_Ejercicio(int opcion) {
     cout << endl;
 }
 
-void ejercicio_4 ()
+void ejercicio_4()
 {
-/*4.	Dados. Escriba un programa para simular el tiro de dos dados. Después debe calcularse la suma de los dos valores. [Nota: cada dado puede
- mostrar un valor entero del 1 al 6, por lo que la suma de los valores variará del 2 al 12, siendo 7 la suma más frecuente, mientras que 2 y 12 
- serán las sumas menos frecuentes]. En la figura 1, se muestran las 36 posibles combinaciones de los dos dados. Su programa debe tirar los dados 
- 36 000 veces. Utilice un arreglo unidimensional para registrar el número de veces que aparezca cada una de las posibles sumas. Imprima los 
- resultados en formato tabular. Determine además si los totales son razonables (es decir, hay seis formas de tirar un 7, por lo que 
- aproximadamente una sexta parte de los tiros deben ser 7).*/
- int limite= 36000, suma;
- int const DIM = 11; 
- int array [DIM]; 
- imprimir(array[], DIM);
-    /*for (int i= 0; i < limite ;i++)
+    /*4.	Dados. Escriba un programa para simular el tiro de dos dados. Después debe calcularse la suma de los dos valores. [Nota: cada dado puede
+     mostrar un valor entero del 1 al 6, por lo que la suma de los valores variará del 2 al 12, siendo 7 la suma más frecuente, mientras que 2 y 12
+     serán las sumas menos frecuentes]. En la figura 1, se muestran las 36 posibles combinaciones de los dos dados. Su programa debe tirar los dados
+     36 000 veces. Utilice un arreglo unidimensional para registrar el número de veces que aparezca cada una de las posibles sumas. Imprima los
+     resultados en formato tabular. Determine además si los totales son razonables (es decir, hay seis formas de tirar un 7, por lo que
+     aproximadamente una sexta parte de los tiros deben ser 7).*/
+    int limite = 36000, suma;
+    int frecuencia[DIM_4] = { 0 };
+
+    for (int i = 0; i < limite; i++)
     {
-        int dado1 = aleatorios(); 
-        int dado2 = aleatorios(); 
-        suma = dado1 + dado2;
-        switch (suma)
-        {
-        case 2:
-            array [0] = 1; 
-            break;
-        
-        default:
-            break;
-        }
+        int dado1 = aleatorios();
+        int dado2 = aleatorios();
+        int suma = dado1 + dado2;
+        frecuencia[suma - 2] += 1; // frecuencia [suma - 2] = frecuencia [suma - 2] + 1, 
+    }
 
-    }*/
-
-
+    cout << " -----------------------------" << endl; 
+    cout << "|    Suma    |  Frecuencia    |" << endl;
+    for (int i = 0; i < DIM_4; i++)
+    {
+        cout << "|      " << setw(6) << left << i + 2 << "|     " << setw(11) << left << frecuencia[i] << "|" <<  endl;
+    }
+    cout << " -----------------------------" << endl;
+    cout << endl; // para mejor visualizacion 
 }
 int aleatorios() {
-  random_device rd;  // Semilla del dispositivo
-  mt19937 gen(rd()); // Inicializar semilla aleatoria
-  int min = 1;
-  int max = 6; // Rangos
-  uniform_int_distribution<int> distribution(min, max);
-  return distribution(gen);
+    random_device rd;  // Semilla del dispositivo
+    mt19937 gen(rd()); // Inicializar semilla aleatoria
+    int min = 1;
+    int max = 6; // Rangos
+    uniform_int_distribution<int> distribution(min, max);
+    return distribution(gen);
 }
-void imprimir (int array [], int DIM)
+void imprimir(int array[DIM_4])
 {
-    for (int i = 0 ; i < DIM ; i++)
+    for (int i = 0; i < DIM_4; i++)
     {
-        cout << array [i] << endl;  
+        cout << array[i] << endl;
     }
 }
