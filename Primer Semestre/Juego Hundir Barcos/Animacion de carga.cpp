a8db0534c9500892eb9d3ef581b6a3a40c4b4604
FernandoHuilca <fernando.huilca@epn.edu.ec>
2023-10-16 16:22:32 -0500

    Aniadiendo colores a la carga

--- Primer Semestre/Juego Hundir Barcos/Animacion de carga.cpp
+++ Primer Semestre/Juego Hundir Barcos/Animacion de carga.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <Windows.h>
+#define color SetConsoleTextAttribute
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 using namespace std;
 
 int main() {
     cout << "Realizando carga en pantalla..." << endl;
-
+    int aux = 0; //para usar los colores sin pasarme del 15 (porque si no se pinta el contorno de las letras y no quiero xd)
     const int numFrames = 30;
     const int frameDelayMs = 100;
 
-    const char frames[] = {'-', '\\', '|', '/'};
+    const char frames[] = { '-', '\\', '|', '/' };
 
-    for (int i = 0; i < numFrames; ++i) {
+    for (int i = 0; i < numFrames; ++i)
+    {
+        aux++;
+        color(hConsole, aux);
+        if (aux == 15)
+        {
+            aux = 0; 
+        }
         cout << "Cargando " << frames[i % 4] << "\r";
         cout.flush();
 
