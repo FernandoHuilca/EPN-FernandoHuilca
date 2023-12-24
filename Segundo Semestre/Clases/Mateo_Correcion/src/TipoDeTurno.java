cc8e5bde258c49b4ab1f7c5618514074cf7d428d
FernandoHuilca <fernando.huilca@epn.edu.ec>
2023-12-23 22:09:46 -0500

    Eliminando cosas del mateo xd

--- Segundo Semestre/Clases/Mateo_Correcion/src/TipoDeTurno.java
+++
@@ -1,4 +0,0 @@
-public enum TipoDeTurno {
-    NORMAL,
-    URGENTE
-}
--- Segundo Semestre/Clases/Netflix_Mateo/src/CuentaNetflix.java
+++
@@ -1,9 +0,0 @@
-public class CuentaNetflix {
-    private Suscripcion tipoDeSuscripcion;
-
-    public CuentaNetflix(String mail, Suscripcion tipoDeSuscripcion) {
-        this.tipoDeSuscripcion = tipoDeSuscripcion;
-
-
-    }
-}
