de4c2c75b5a9bbbfda7fb1b62bbf3d30bf4b2fe8
FernandoHuilca <fernando.huilca@epn.edu.ec>
2023-12-27 21:01:53 -0500

    Subiendo avance del chat online

--- Segundo Semestre/Practicas Personales/Chat_Practica/src/Main.java
+++ Segundo Semestre/Practicas Personales/Chat_Practica/src/Main.java
@@ -1,5 +1,6 @@
 // Author: Fernando Huilca
 public class Main {
+    public static void main(String[] args) {
 
     }
 }
--- Segundo Semestre/Practicas Personales/Chat_online/Chat_online/Chat_online.cpp
+++ Segundo Semestre/Practicas Personales/Chat_online/Chat_online/Chat_online.cpp
@@ -1,13 +1,52 @@
 // Author: Fernando Huilca
 #include <iostream>
 #include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
+
+
+//Librerias
+void presentarListaDeAmigos();
+int presentarMenuPrincipal();
 
 
 
 int main()
 {
-    cout << "___________Chat Secreto___________" << endl; 
-    cout << "1. Mostrar amigos " << endl; 
-    cout << "0. Salir" << endl;
+    
+    switch (presentarMenuPrincipal())
+    {
+    case 1: 
+    {
+        presentarListaDeAmigos();
+    }
+    default:
+        break;
+    }
+
+}
+
+void presentarListaDeAmigos() {
+    ofstream leerArchivo;
+    leerArchivo.open("ListaDeAmigos.txt");
+    if (!leerArchivo.is_open())
+    {
+        cout << "Error: No se pudo abrir el archivo de amigos";
+    }
+    else
+    {
+
+    }
+}
+int presentarMenuPrincipal() {
+    int opcion; 
+    do {
+        cout << "___________Chat Secreto___________" << endl;
+        cout << "1. Mostrar amigos " << endl;
+        cout << "0. Salir" << endl;
+        cin >> opcion;
+    } while (opcion < 0 || opcion > 1);
+    return opcion; 
 }
